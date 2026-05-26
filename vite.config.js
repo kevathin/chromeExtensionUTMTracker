9947f5a0246613d9c0df079084b5753ad2384c4e
Kevin Bertasius <webmaster@konstructdigital.com>
2026-05-26 15:17:56 -0600

    attempt to package js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                background: 'src/background.js'
+            },
+            output: {
+                entryFileNames: '[name].js',
+            }
+        }
+    }
+});
